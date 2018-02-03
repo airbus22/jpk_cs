2400cbbcae97330ab5e9a2425fc3dfef7e2df73f
GB-hp <grzegorz.brodowski@outlook.com>
2018-02-03 23:02:13 +0100

    jakieś podejście z błędami

--- jpk_main_window.cs
+++ jpk_main_window.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace jpkapp
 {
@@ -19,6 +20,32 @@ namespace jpkapp
 
         private void button1_Click(object sender, EventArgs e)
         {
+            
+        }
+
+        public class dbs
+        {
+            private String connectionString;
+            private String OleDBProvider = "Microsoft.JET.OLEDB.4.0"; \\if ACE Microsoft.ACE.OLEDB.12.0
+            private String OleDBDataSource = "C:\\yourdb.mdb";
+            private String OleDBPassword = "infosys";
+            private String PersistSecurityInfo = "False";
+
+            public dbs()
+            {
+
+            }
+
+            public dbs(String connectionString)
+            {
+                this.connectionString = connectionString;
+            }
+
+            public String konek()
+            {
+                connectionString = "Provider=" + OleDBProvider + ";Data Source=" + OleDBDataSource + ";JET OLEDB:Database Password=" + OleDBPassword + ";Persist Security Info=" + PersistSecurityInfo + "";
+                return connectionString;
+            }
 
         }
     }
