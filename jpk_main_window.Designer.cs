2ec87af5c57cda9d0e25bd32957fbc67a1a22747
GB-dell <grzegorz.brodowski@outlook.com>
2018-02-14 22:59:53 +0100

    potrzeba jeszcze dopracować szczegóły logiki aplikacji

--- jpk_main_window.Designer.cs
+++ jpk_main_window.Designer.cs
@@ -33,11 +33,12 @@
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.button1.Location = new System.Drawing.Point(62, 71);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(267, 89);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Generuj plik JPK MAG";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
