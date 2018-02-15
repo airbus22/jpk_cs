95a4913416e0b90572d28111205a2c1e877510a0
GB-dell <grzegorz.brodowski@outlook.com>
2018-02-15 17:07:27 +0100

    dalej GUI

--- jpk_main_window.Designer.cs
+++ jpk_main_window.Designer.cs
@@ -42,7 +42,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button1.Location = new System.Drawing.Point(130, 162);
+            this.button1.Location = new System.Drawing.Point(132, 185);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(267, 89);
             this.button1.TabIndex = 0;
